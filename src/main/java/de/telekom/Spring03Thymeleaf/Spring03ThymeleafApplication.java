2cde6933cacf0de5ad7b54c41b6d41e785d1b0c2
mkrill <michael.krill@telekom.de>
2019-12-09 13:42:55 +0100

    1st commit

---
+++ src/main/java/de/telekom/Spring03Thymeleaf/Spring03ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package de.telekom.Spring03Thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring03ThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring03ThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/telekom/Spring03Thymeleaf/Spring03ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telekom.Spring03Thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring03ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
