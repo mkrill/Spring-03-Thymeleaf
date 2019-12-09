5500a5a9b14b32f6050bcb6814a78e71b1844044
mkrill <michael.krill@telekom.de>
2019-12-09 14:10:29 +0100

    content configured correctly: home.html into templates-dir

--- src/main/java/de/telekom/Spring03Thymeleaf/Spring03ThymeleafApplication.java
+++ src/main/java/de/telekom/Spring03Thymeleaf/Spring03ThymeleafApplication.java
@@ -2,7 +2,12 @@ package de.telekom.Spring03Thymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 @SpringBootApplication
 public class Spring03ThymeleafApplication {
 
@@ -10,4 +15,11 @@ public class Spring03ThymeleafApplication {
 		SpringApplication.run(Spring03ThymeleafApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String home(Model model,
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		model.addAttribute("name", name);
+
+		return "home";
+	}
 }
